d6f5ba1403a2f0c8bee2d826ddb530b8c170e7db
Alexis Pequeño <alxpequeno@gmail.com>
2019-01-03 09:25:36 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into clientes(id,nombre,apellido,email,create_at) values (null, 'Andres', 'Guzman','profesor@exam.co','2018-01-01');
+insert into clientes(id,nombre,apellido,email,create_at) values (null, 'Lex', 'Musante','lex@exam.co','2018-11-11');
